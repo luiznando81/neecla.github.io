3a048a0b06ba5e9658256b442fb7fb17c752b408
davibicudo <28100741+davibicudo@users.noreply.github.com>
2018-10-18 11:40:32 +0200

    site beta

--- _site.R
+++ _site.R
@@ -4,10 +4,5 @@ suppressPackageStartupMessages({
   library(stringr)
 })
 
-## variables I need for my site 
-data <- readr::read_csv('important_data.csv')
-
-
 ## knitr options I want set as default for all ('global') code chunks
 knitr::opts_chunk$set(echo = FALSE, message = FALSE, warning = FALSE)
-
